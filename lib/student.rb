350cc88ccaeced8b3d06f5c0d786fb5c8f63812d
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-09 19:06:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,26 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(attributes)
+    attributes.each_pair { |key, value| self.send(("#{key}="), value) }
+    @@all << self
+    #binding.pry
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(attributes)
+    attributes.each { |student_hash| self.new(student_hash) }
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(attributes)
+    attributes.each_pair { |key, value| self.send(("#{key}="), value) }
   end
 
   def self.all
-    
+    @@all
   end
 end
 
