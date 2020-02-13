828e2f1f1922bd5bf9a83c9a0bc9ee2b109fbea2
Steven Perfors <stevecanbuild@gmail.com>
2020-02-13 18:35:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,7 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      profile_path = student.css("a").attribute("href").value
-      student_details[:profile_url] = "./fixtures/student-site/" + profile_path
+      student_details[:profile_url] = student.css("a").attribute("href").value
       students << student_details
     end
     students
